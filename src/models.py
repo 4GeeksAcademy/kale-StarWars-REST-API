9ac8f5be2f44209291957ea58bed1f3d0d5db592
KaleCordero <kalecascante61@gmail.com>
2025-01-26 02:44:19 +0000

    correcciones

--- src/models.py
+++ src/models.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Person(db.Model):
+    __tablename__ = 'person'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     hair_color = db.Column(db.String(80), nullable=False)
@@ -14,7 +15,22 @@ class Person(db.Model):
     height = db.Column(db.Integer, nullable=False)
     mass = db.Column(db.Integer, nullable=False)
 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'hair_color': self.hair_color,
+            'eye_color': self.eye_color,
+            'skin_color': self.skin_color,
+            'gender': self.gender,
+            'specie': self.specie,
+            'home_world': self.home_world,
+            'height': self.height,
+            'mass': self.mass
+        }
+
 class Planet(db.Model):
+    __tablename__ = 'planet'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     diameter = db.Column(db.Integer, nullable=False)
@@ -26,18 +42,56 @@ class Planet(db.Model):
     climate = db.Column(db.String(80), nullable=False)
     terrain = db.Column(db.String(80), nullable=False)
 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'diameter': self.diameter,
+            'rotation_period': self.rotation_period,
+            'orbital_period': self.orbital_period,
+            'population': self.population,
+            'surface_water': self.surface_water,
+            'gravity': self.gravity,
+            'climate': self.climate,
+            'terrain': self.terrain
+        }
+
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(80), nullable=False)
     password = db.Column(db.String(80), nullable=False)
 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email
+        }
+
 class FavoritePeople(db.Model):
+    __tablename__ = 'favorite_people'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     person_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'person_id': self.person_id
+        }
+
 class FavoritePlanet(db.Model):
+    __tablename__ = 'favorite_planets'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'planet_id': self.planet_id
+        }
